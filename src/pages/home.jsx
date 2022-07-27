7a3e1714df11860c1d4273df5d3d8e26ae70d8de
DuhaAbdullah <do7a.ab@gmail.com>
2022-07-27 19:09:12 +0500

    7/27/2022

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,36 +1,133 @@
 import NavBar from "../components/navBar";
+import Button from "../common/button";
+import { Link, useNavigate } from "react-router-dom";
+import GlobeIcon from "../icons/globeIcon";
+import TwitterIcon from "../icons/twitterIcon";
+import FacebookIcon from "../icons/facebookIcon";
+import InstagramIcon from "../icons/instagram.Icon";
+import YoutubeIcon from "../icons/youtubeIcon";
 
 function Home() {
+  const navigate = useNavigate();
+  function handleClick(e) {
+    navigate("/menu");
+  }
+
   return (
     <div>
       <NavBar />
-      <div className="home-image-container">
-        <img className="home-image" src="images/cappuccino.webp" />
-        <p className="home-p">
-          "Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum"
+      <div className="home-cta-container">
+        <img src="images\home-background.webp" className="cta-image" />
+        <p className="home-p-1">FIND THE COFFEE FOR YOU</p>
+        <p className="home-p-2">Doha Coffee at home</p>
+        <p className="home-p-3">
+          Enjoy the coffee you love, at our place or yours.
+        </p>
+        <Button className="menu-button" onClick={handleClick}>
+          Go to menu page
+        </Button>
+      </div>
+      <div className="home-content">
+        <p className="home-p-4"> DOHA® COFFEE. MAKE IT YOURS AT HOME.</p>
+        <p className="home-p-5">
+          Discover our Doha® coffee made truly yours at home. Enjoy a fresh cup
+          of 100%, Arabica coffee or an inspired café classic, whenever and
+          however you like it.
         </p>
       </div>
-      <div className="home-item-container">
-        <p className="home-p">
-          "Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum"
+      <div className="home-banner">
+        <img
+          className="home-banner-image"
+          src="images/home-background-2.webp"
+        />
+        <p className="home-p-6">FIND YOUR ROAST</p>
+        <p className="home-p-7">Roast</p>
+        <p className="home-p-8">Spectrum</p>
+        <p className="home-p-9">
+          Discover the different coffee and flavour profiles within our
+          Starbucks<sup>®</sup> Roast Spectrum, with 3 roast categories to
+          explore, find your perfect coffee.
         </p>
-        <img className="home-image" src="images/chocolate-brew.webp" />
+      </div>
+      <div className="home-about-us-container">
+        <div className="about-us-image">
+          <img src="images/about-us-image.webp" />
+        </div>
+        <div className="about-us-content">
+          <p className="home-p-10">ABOUT DOHA</p>
+          <p className="home-p-11">About us</p>
+          <p className="home-p-12">
+            Discover what makes Starbucks® unique, from our commitment to human
+            connection and quality coffee, to our welcoming coffeehouses and
+            delicious coffees you can enjoy at home.
+          </p>
+        </div>
+      </div>
+      <div className="roast-content-container">
+        <div className="roast-content">
+          <p className="home-p-10 light">CRAFT</p>
+          <p className="home-p-11 light">
+            The roast <br /> Spectrum
+          </p>
+
+          <p className="home-p-12 light">
+            Which roast profile do you prefer? Starbucks® Blonde Roast, Medium
+            Roast or Dark Roast? These three make up the Starbucks® roast
+            spectrum.
+          </p>
+        </div>
+        <div className="roast-image-container">
+          <img className="roast-image" src="images/home-background-3.webp" />
+        </div>
+      </div>
+      <div className="swiper-container"></div>
+      <div className="footer-container">
+        <div className="footer-logo-container">
+          <a href="/" className="home-logo-text">
+            Doha Coffee
+          </a>
+          <p className="home-p-13 light">DOHA COFFEE AT HOME</p>
+        </div>
+        <div className="home-links-container">
+          <Link className="home-p-14 light" to="/">
+            <p>About us</p>
+          </Link>
+          <Link className="home-p-14 light" to="/">
+            <p>Contact us</p>
+          </Link>
+          <div className="home-location-container">
+            <GlobeIcon className="globe-svg" />
+            <Link className="home-p-14 light" to="/">
+              <p>English</p>
+            </Link>
+          </div>
+        </div>
+        <div className="footer-legal-links">
+          <Link className="home-p-15" to="/">
+            <p>Cookies</p>
+          </Link>
+          <Link className="home-p-15" to="/">
+            <p>Privacy Policy</p>
+          </Link>
+          <Link className="home-p-15" to="/">
+            <p>Terms of use</p>
+          </Link>
+        </div>
+        <div className="social-accounts-container">
+          <TwitterIcon className="social-account" />
+          <FacebookIcon className="social-account" />
+          <InstagramIcon className="social-account" />
+          <YoutubeIcon className="social-account" />
+        </div>
+        <div>
+          <p className="home-p-16">
+            Nestlé uses Doha trademarks under license. Pike Place is a
+            registered trademark of The Pike Place Market PDA, used under
+            license. Nespresso® and NESCAFÉ® Dolce Gusto® are registered
+            trademarks of Société de Produits Nestlé S.A.. All other trademarks
+            are the property of their owners.
+          </p>
+        </div>
       </div>
     </div>
   );
